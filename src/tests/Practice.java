1666a71b01b2754800260a4302e4df72e168950c
İsmail Özcan <50797929+Isso-Chan@users.noreply.github.com>
2020-05-12 17:03:41 +0200

    Update Practice.java

--- src/tests/Practice.java
+++ src/tests/Practice.java
@@ -5,5 +5,10 @@ public class Practice {
     public static void main(String[] args) {
 
         System.out.println("Today is good");
+        
+        
+        
+        
+        // I added this part in github webpage
     }
 }
